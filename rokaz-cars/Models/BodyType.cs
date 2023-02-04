c3865d3ddb094ab50a0d2e73d4d4a87077f7b950
Kirill Koval <brcyrill2@gmail.com>
2023-02-04 15:29:43 +0200

    Add project files.

---
+++ rokaz-cars/Models/BodyType.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace rokaz_cars.Models
+{
+    public class BodyType
+    {
+        [Key]
+        public long Id { get; set; }
+        public string? name { get; set; }
+    }
+}
---
+++ rokaz-cars/Models/Car.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace rokaz_cars.Models
+{
+    public class Car
+    {
+        [Key]
+        public long Id { get; set; }
+        public string? mark { get; set; }
+        public string? model { get; set; }
+        public string? year { get; set; }
+        public int? body_type_id { get; set; }
+        public string? color { get; set; }
+        public int? mileage { get; set; }
+        public long? cost { get; set; }
+    }
+}
